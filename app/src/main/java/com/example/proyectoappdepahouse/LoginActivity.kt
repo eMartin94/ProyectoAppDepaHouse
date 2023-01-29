620e66eead538c3bfffb63dcecf81a40559e8405
Martin Pizango <eduardo.martin688@gmail.com>
2023-01-28 22:46:39 -0500

    Agregando Login con Google

--- app/src/main/java/com/example/proyectoappdepahouse/LoginActivity.kt
+++ app/src/main/java/com/example/proyectoappdepahouse/LoginActivity.kt
@@ -7,7 +7,13 @@ import android.util.Log
 import android.widget.Toast
 import com.example.proyectoappdepahouse.databinding.ActivityLoginBinding
 import com.example.proyectoappdepahouse.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -15,6 +21,7 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var b:ActivityLoginBinding
+    private lateinit var clientGoogle:GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(b.root)
 
         auth = Firebase.auth
+
+        val opt = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        clientGoogle = GoogleSignIn.getClient(this, opt)
 
         b.btnIniciarsesion.setOnClickListener {
 
@@ -55,6 +68,12 @@ class LoginActivity : AppCompatActivity() {
             startActivity(i)
         }
 
+        b.btnGoogle.setOnClickListener {
+
+            val signInIntent = clientGoogle.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
     }
 
     public override fun onStart() {
@@ -63,6 +82,22 @@ class LoginActivity : AppCompatActivity() {
         val currentUser = auth.currentUser
         if(currentUser != null){
             reload();
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w("TAG", "Google sign in failed", e)
+                // Update UI accordingly
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -91,5 +126,26 @@ class LoginActivity : AppCompatActivity() {
 
         val i = Intent(this, MainActivity::class.java)
         startActivity(i)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Inicio de sesión con éxito, actualizar UI con la información del usuario
+                    reload()
+                } else {
+                    // Si falla el inicio de sesión, mostrar un mensaje de error al usuario
+                    Toast.makeText(
+                        baseContext, "Error al Iniciar Sessión con Google",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
